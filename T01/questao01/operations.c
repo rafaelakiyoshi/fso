ae33d2dc290e7a5146ea3243817a3c0a0139300c
Rafael Akiyoshi <rafah94@hotmail.com>
2017-03-19 13:45:49 -0300

    creating mark of function distance of 2 dots

---
+++ T01/questao01/operations.c
@@ -0,0 +1,15 @@
+#include "operations.h"
+#include "types.h"
+#include <math.h>
+
+int is_convex(Quadrilateral* quadrilateral) {
+
+}
+
+double perimeter(Quadrilateral* quadrilateral) {
+
+}
+
+double area(Quadrilateral* quadrilateral) {
+  
+}
--- T01/questao01/operations.h
+++ T01/questao01/operations.h
@@ -3,7 +3,8 @@
 #include "types.h"
 #include <math.h>
 
-int is_convex(Quadrilateral* quadrilateral);
+double distanceBetweenDots(Dot* one, Dot* two); 
+bool isConvex(Quadrilateral* quadrilateral);
 double perimeter(Quadrilateral* quadrilateral);
 double area(Quadrilateral* quadrilateral);
 #endif
