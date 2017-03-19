60cd0e0d8586d4022977271fb080c87bdc336e99
Rafael Akiyoshi <rafah94@hotmail.com>
2017-03-19 13:35:36 -0300

    creating .h of operations

---
+++ T01/questao01/operations.h
@@ -0,0 +1,9 @@
+#ifndef OPERATIONS_H
+#define OPERATIONS_H
+#include "types.h"
+#include <math.h>
+
+int is_convex(Quadrilateral* quadrilateral);
+double perimeter(Quadrilateral* quadrilateral);
+double area(Quadrilateral* quadrilateral);
+#endif
